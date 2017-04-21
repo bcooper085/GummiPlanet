290dba96e3d11409b5f667d1c8978570e35cf357
bcooper085 <bcooper085@gmail.com>
2017-04-21 09:26:25 -0700

    add product and context models and default server connection

---
+++ src/GummiPlanet/Models/GummiPlanetDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GummiPlanet.Models
+{
+    public class GummiPlanetDbContext : DbContext
+    {
+        public GummiPlanetDbContext()
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=GummiPlanet;integrated security=True");
+        }
+
+        public GummiPlanetDbContext(DbContextOptions<GummiPlanetDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
